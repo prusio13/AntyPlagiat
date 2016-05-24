9ddf36d902f7568e03e19bdd694aeae326d62352
osboxes.org <brylat@gmail.com>
2016-05-24 04:23:03 +0100

    zmiany

--- analiza tekstu/text_comparing.py
+++ analiza tekstu/text_comparing.py
@@ -1,43 +1,48 @@
 import difflib
 import Levenshtein
-f1path = "text1.txt"
-f2path = "text2.txt"
 
-file1=open(f1path,"r")
-file2=open(f2path,"r")
+f1path = "data/reportToCheck.txt"
+f2path = ""
+file1=open(f1path,"r", encoding='iso-8859-2')
 text1 = file1.read()
-text2 = file2.read()
 
-matching_blocks = list()
-sm = difflib.SequenceMatcher(None,text1,text2)
-matching_block_number = 0
-matching_blocks_length = 0
-max_length = (len(text1) if (len(text1) > len(text2)) else len(text1))
-for block_info in sm.get_matching_blocks():          #zwraca 3-el tablice zaw. pozycje bloku w pierwszym tekscie, drugim tekscie i dlugosc bloku
-    #print ("a[%d] and b[%d] match for %d elements" % block)
-    if(block_info[2]>0):
-        print("---------------------------------------------------")
-        block = text1[block_info[0]:block_info[0] + block_info[2]]
-        print(block,"\nDlugosc bloku:", block_info[2])
-        matching_block_number += 1
-        matching_blocks_length += block_info[2]
-        matching_blocks.append(block)
+import os
 
-#sm2 = difflib.SequenceMatcher(None,text2,text1,autojunk=False)
-# for block_info in sm.get_matching_blocks():          #zwraca 3-el tablice zaw. pozycje bloku w drugim tekscie, drugim tekscie i dlugosc bloku
-#     if(block_info[2]>0):
-#         print(block_info)
-#         block = text1[block_info[1]:block_info[1] + block_info[2]]
-#         if block not in matching_blocks:
-#             print("---------------------------------------------------")
-#             print(block,"Dlugosc bloku: ", block_info[2])
-#             matching_block_number += 1
-#             matching_blocks_length += block_info[2]
-#             matching_blocks.append(block)
-print("---------------------------------------------------")
-print("Odleglosc Levenshteina: ",Levenshtein.distance(text2,text1)) #ile operacji na znakach nalezy wykonac aby z jedengo tekstu otrzymac drugi
-print(Levenshtein.seqratio(text1,text2))
-print("wykrytych blokow:", matching_block_number,"- %d" %matching_blocks_length,"/ %d znakow w tekscie" %len(text2),"-","%d%% pokrycia"%(matching_blocks_length/len(text2)*100))
+files_in_dir = os.listdir("data/reports/")
+for file_in_dir in files_in_dir:
+    f2path = "data/reports/" + file_in_dir
+    file2 = open(f2path, "r", encoding='iso-8859-2')    #kodowanie plikow windowsowych
+    text2 = file2.read()
+    print ("Scanning file:", file_in_dir)
+
+    matching_blocks = list()
+    sm = difflib.SequenceMatcher(None, text1, text2)
+    matching_block_number = 0
+    matching_blocks_length = 0
+    max_length = (len(text1) if (len(text1) > len(text2)) else len(text1))
+    for block_info in sm.get_matching_blocks():  # zwraca 3-el tablice zaw. pozycje bloku w pierwszym tekscie, drugim tekscie i dlugosc bloku
+        # print ("a[%d] and b[%d] match for %d elements" % block)
+        if (block_info[2] > 50):
+            print("---------------------------------------------------")
+            block = text1[block_info[0]:block_info[0] + block_info[2]]
+            print(block)
+            print("\nDlugosc bloku:", block_info[2])
+            matching_block_number += 1
+            matching_blocks_length += block_info[2]
+            matching_blocks.append(block)
+
+    #print("Odleglosc Levenshteina: ", Levenshtein.distance(text2, text1))  # ile operacji na znakach nalezy wykonac aby z jedengo tekstu otrzymac drugi
+    #print(Levenshtein.seqratio(text1, text2))
+    print("wykrytych blokow:", matching_block_number, "- %d" % matching_blocks_length,
+          "/ %d znakow w tekscie" % len(text2), "-", "%d%% pokrycia" % (matching_blocks_length / len(text2) * 100))
+    file2.close()
+    print("#####################################################")
+
+
+# file2 = open(f2path, "r", encoding='iso-8859-2')
+# text2 = file2.read()
+# print(text2)
+
 file1.close()
-file2.close()
+
 
